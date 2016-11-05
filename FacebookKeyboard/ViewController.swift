965dbb5b2088deeb374322d5af03fb8151195825
Adam Noffsinger <adam.noffsinger@uber.com>
2016-11-05 18:09:22 +0100

    Initial commit

--- FacebookKeyboard/ViewController.swift
+++ FacebookKeyboard/ViewController.swift
@@ -10,10 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginButton: UIButton!
+    
+    var originalButtonCenter: CGPoint!
+    var offsetButtonCenter: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        originalButtonCenter = loginButton.center
+        offsetButtonCenter = cgpoint
+        
+        NotificationCenter.default.addObserver(forName: NSNotification.Name.UIKeyboardWillShow , object: nil, queue: OperationQueue.main) { (Notification) in
+            print ("keyboard bitch")
+            self.loginButton.center = offsetButtonCenter
+        }
+        
+        NotificationCenter.default.addObserver(forName: NSNotification, selector: <#T##Selector#>, name: <#T##NSNotification.Name?#>, object: <#T##Any?#>)
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
